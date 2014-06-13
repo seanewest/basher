3940ca545f4f81553421209acfed40b02722c3b5
Sean West <sean.e.west@gmail.com>
2014-06-13 02:45:45 -0400

    cleaned up cmd.js

--- bin/cmd.js
+++ bin/cmd.js
@@ -10,37 +10,22 @@ var resolve = require('resolve').sync;
 var basher = require('../')
 
 var basedir = process.cwd();
-var basherpath;
 var args = process.argv.slice(2);
 
-function resolve_using_filepath(filepath) {
-  try {
-    basherpath = resolve('./' + filepath, {basedir: basedir});
-  } catch (ex) {
-    if (!basherpath) {
-      console.error('unable to resolve ' + filepath);
-      process.exit(1)
-    }
-  }
+var basherfile = './Basherfile'
+var path = (args[0] !== undefined) 
+  ? args[0]
+  : basherfile
+
+try {
+  path = resolve('./' + path, {basedir: basedir});
+} catch (ex) {
+  if (path === basherfile)
+    console.error('Unable to use local Basherfile.');
+  else
+    console.error('Unable to resolve ' + path);
+  process.exit(1)
 }
 
-function resolve_using_basherfile() {
-  try {
-    basherpath = resolve('./Basherfile', {basedir: basedir});
-  } catch (ex) {
-    if (!basherpath) {
-      console.error('Unable to find local Basherfile.');
-      process.exit(1)
-    }
-  }
-}
-
-if (args[0] !== undefined) {
-  resolve_using_filepath(args[0])
-} else {
-  resolve_using_basherfile()
-}
-
-var basher = require('../')
-var basher_methods = require(basherpath)
-basher(basher_methods)
+var module_of_methods = require(path)
+basher(module_of_methods)
